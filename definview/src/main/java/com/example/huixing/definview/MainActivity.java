8aa30ec0cfc18f42cd52a95230cfa0433985da06
dogoodsoft-APP <wangxiabing@001.local>
2017-09-21 11:15:47 +0800

    加入一个计时器自定义view

--- definview/src/main/java/com/example/huixing/definview/MainActivity.java
+++ definview/src/main/java/com/example/huixing/definview/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.huixing.definview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TomatoView clockView;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clockView = (TomatoView) findViewById(R.id.clockView);
+        textView = (TextView) findViewById(R.id.tv_start);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clockView.start();
+            }
+        });
     }
 }
