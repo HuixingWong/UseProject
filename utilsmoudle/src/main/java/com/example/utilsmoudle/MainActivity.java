3ec5b3fe035557fa88879151300c5349b7c939c1
dogoodsoft-APP <wangxiabing@001.local>
2017-10-10 17:15:00 +0800

    Excel中数据的读写

--- utilsmoudle/src/main/java/com/example/utilsmoudle/MainActivity.java
+++ utilsmoudle/src/main/java/com/example/utilsmoudle/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,14 +21,24 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+/**
+ *  里面加入了Excel中数据的读写
+ *  比如把数据生成Excel
+ *  从Excel中读取数据等等
+ */
 
 public class MainActivity extends AppCompatActivity implements TestCallback.OnFragmentInteractionListener {
 
     private TextView tv;
     private Button mXlsBtn;
+
+    private Button mXlsRead;
+
 
     String[] str = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
     String filename = "textExcel.xls";
@@ -42,6 +53,15 @@ public class MainActivity extends AppCompatActivity implements TestCallback.OnFr
             @Override
             public void onClick(View v) {
                 exportExcelFile();
+            }
+        });
+
+        mXlsRead = (Button) findViewById(R.id.read_xls_btn);
+        mXlsRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                readXlsToProject(MainActivity.this);
             }
         });
 
@@ -60,36 +80,84 @@ public class MainActivity extends AppCompatActivity implements TestCallback.OnFr
         fragmentTransaction.add(R.id.container_a_c, testCallback);
         fragmentTransaction.commit();
 
-
     }
-    public void exportExcelFile2(){
-        int size = 10;
-        Workbook wb = new HSSFWorkbook();
-        Sheet sh = wb.createSheet();
 
-        for(int rownum=0;rownum<size;rownum++){
-            Row row = sh.createRow(rownum);
-            for(int cellnum=0;cellnum<10;cellnum++){
-                Cell cell = row.createCell(cellnum);
-                cell.setCellValue(str[cellnum]);
-            }
-        }
+    /**
+     * 从excel中读取数据的方法
+     *
+     * @param context
+     */
+    public void readXlsToProject(Context context) {
 
         try {
-            FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
-            wb.write(fos);
-            fos.close();
-            Toast.makeText(getApplicationContext(),"导出成功",Toast.LENGTH_SHORT).show();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+            File file = new File(Environment.getExternalStorageDirectory() + "/abc.xls");
+            FileInputStream ios = new FileInputStream(file);
+
+            Workbook book = new HSSFWorkbook(ios);
+            int numberOfSheets = book.getNumberOfSheets();
+
+            Sheet sheet = book.getSheetAt(0);
+
+            String sheetName = sheet.getSheetName();
+
+            /**
+             * 获取到excel的行数，全部行数。
+             */
+            int physicalNumberOfRows = sheet.getPhysicalNumberOfRows();
+
+            int sum = 0;
+
+            for (int i = 0; i < physicalNumberOfRows; i++) {
+
+
+                /**
+                 * 获取某一行一共有多少列，包括空的
+                 */
+                int columnWidth = sheet.getColumnWidth(i);
+
+                /**
+                 * 获取某一行有多少列有内容
+                 */
+                int defaultColumnWidth = sheet.getDefaultColumnWidth();
+
+                Row row = sheet.getRow(i);
+
+                for (int j = 0; j < defaultColumnWidth; j++) {
+
+
+                    Cell cell = row.getCell(j);
+                    String s = cell.toString();
+
+                    Log.e("123", "sum: " + sum + " i:" + i + " j:" + j + "  stirng:  " + s);
+                    sum += 1;
+
+                }
+
+
+            }
+
+
+            Toast.makeText(context, "表的数量：" + numberOfSheets + "   表的名字：" +
+                            sheetName + "    physicalNumberOfRows:" + physicalNumberOfRows,
+                    Toast.LENGTH_SHORT).show();
+
+            book.close();
+
+
         } catch (IOException e) {
+            Log.e("123", "出错了");
             e.printStackTrace();
         }
+
+
     }
 
-
+    /**
+     * 将数据写入Excel的方法
+     */
     public void exportExcelFile() {
-        int size = 10;
+        int size = 100;
         Workbook wb = new HSSFWorkbook();
         Sheet sh = wb.createSheet();
 
@@ -103,7 +171,7 @@ public class MainActivity extends AppCompatActivity implements TestCallback.OnFr
 
         try {
             File file = new File(Environment.getExternalStorageDirectory() + "/abc.xls");
-            if (!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
             }
             FileOutputStream fos = new FileOutputStream(file);
